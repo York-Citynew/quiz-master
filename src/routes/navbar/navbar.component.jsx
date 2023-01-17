496db2641c5b4fbfae695d87da8a55a5a403624c
York-Citynew <y.c.new.a@gmail.com>
2023-01-18 01:17:23 +0330

    updated navbar buttons

--- src/routes/navbar/navbar.component.jsx
+++ src/routes/navbar/navbar.component.jsx
@@ -5,10 +5,20 @@ const Navbar = () => {
   return (
     <>
       <div className='navbar-container'>
-        <h1>Quiz Master</h1>
-        <span>
-          <Link to={"about"}>About me</Link>
-        </span>
+        <div className='logo-container'>
+          <h1>Quiz Master</h1>
+        </div>
+        <div className='links-container'>
+          <span>
+            <Link to={"about"}>About me</Link>
+          </span>
+          <span>
+            <Link to={"leaderboard"}>leaderboard</Link>
+          </span>
+          <span>
+            <button className='sign-up-button'>sign in</button>
+          </span>
+        </div>
       </div>
       <Outlet />
     </>
