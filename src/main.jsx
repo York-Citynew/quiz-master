36d346f8de70f1d31a073a8a6e886c59477e3591
York-Citynew <y.c.new.a@gmail.com>
2023-01-21 14:29:57 +0330

    disabled strict mode. there was an issue with useEffect's functionlaity

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,14 @@ import { Provider } from "react-redux";
 import store, { persistor } from "./utils/store/store";
 import { PersistGate } from "redux-persist/integration/react";
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate
-        loading={null}
-        persistor={persistor}
-      >
-        <App />
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>
+  // <React.StrictMode> temporary disabled strict mode because of useEffect malfunctioning
+  <Provider store={store}>
+    <PersistGate
+      loading={null}
+      persistor={persistor}
+    >
+      <App />
+    </PersistGate>
+  </Provider>
+  // </React.StrictMode>
 );
