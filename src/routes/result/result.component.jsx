7cb192227963c7a0804374a5c9eff6e5eb78a703
York-Citynew <y.c.new.a@gmail.com>
2023-01-27 17:35:21 +0330

    minor changes. added some temporary patches for recent changes

--- src/routes/result/result.component.jsx
+++ src/routes/result/result.component.jsx
@@ -5,82 +5,97 @@ import { setQuizData } from "../../utils/store/features/quiz/quiz-slice";
 import Button, { BUTTON_TYPES } from "../../components/button/button.component";
 import Modal from "../../components/modal/modal.component";
 import "./result.styles.scss";
-import { addUserRecord } from "../../utils/firebase/firebase.utils";
-
+import Spinner from "../../components/spinner/spinner.component";
+import {
+  addUserRecord,
+  deleteUserQuiz,
+  getUserAnswers,
+} from "../../utils/firebase/firebase.utils";
+const scoreCalculator = (difficulty, duration, percentage) => {
+  const normalScore = duration * percentage;
+  switch (difficulty) {
+    case "hard":
+      return 2 * normalScore;
+    case "medium":
+      return 1.5 * normalScore;
+    case "easy":
+      return normalScore;
+    default:
+      break;
+  }
+};
 const Result = () => {
+  let correctAnswersAmount = null;
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const navigateHandler = () => {
     navigate("/");
   };
-  const { quizData } = useSelector((state) => state.quiz);
-  const { category, difficulty, duration, tests } = quizData;
+  const { quizTests, quizAnswers, isLoading } = useSelector(
+    (state) => state.quiz
+  );
+  const { category, difficulty, duration, tests } = quizTests;
   const { user } = useSelector((state) => state.user);
-  const scoreCalculator = (difficulty, duration, percentage) => {
-    const normalScore = duration * percentage;
-    switch (difficulty) {
-      case "hard":
-        return 2 * normalScore;
-      case "medium":
-        return 1.5 * normalScore;
-      case "easy":
-        return normalScore;
-      default:
-        break;
-    }
-  };
-  const correctAnswers = quizData.tests.filter(
-    (item) => item.selectedValue === item.correct_answer
-  ).length;
-
   useEffect(() => {
-    const asyncFunc = async () => {
-      await addUserRecord(
-        user,
-        scoreCalculator(difficulty, duration, correctAnswers / tests.length),
-        category
-      );
-    };
-    asyncFunc();
-    if (user) asyncFunc();
+    if (user) {
+      (async () => {
+        correctAnswersAmount = await quizTests.tests.filter(
+          (item, index) => item.selectedValue === quizAnswers[index]
+        ).length;
+        console.log(correctAnswersAmount);
+        await addUserRecord(
+          user,
+          scoreCalculator(
+            difficulty,
+            duration,
+            correctAnswersAmount / tests.length
+          ),
+          category
+        );
+      })();
+    }
     return () => {
+      deleteUserQuiz(user); //Hoist it on componentMount
       dispatch(setQuizData([])); //not working properly in strict mode
       //not working(again)
     };
   }, []);
+  if (isLoading) return <Spinner />;
+
   return (
     <Modal>
       <div className='result-container'>
-        {quizData.tests.map((item) => (
+        {quizTests.tests.map((item, index) => (
           <div
             key={item.question}
             className='test-result-container'
           >
             <span className='test-question'>{item.question}</span>
             <ul>
-              {[item.correct_answer, ...item.incorrect_answers].map(
-                (option) => {
-                  return (
-                    <li
-                      className={`test-options ${
-                        item.selectedValue === option &&
-                        item.selectedValue === item.correct_answer
-                          ? "win"
-                          : item.selectedValue === option
-                          ? "correct"
-                          : "loss"
-                      }`}
-                      key={option}
-                    >
-                      {option}
-                    </li>
-                  );
-                }
-              )}
+              {item.options.map((option) => {
+                return (
+                  <li
+                    className={`test-options ${
+                      item.selectedValue === option &&
+                      item.selectedValue /*item.correct_answer*/ ===
+                        quizAnswers[index]
+                        ? "win"
+                        : item.selectedValue === option
+                        ? "correct"
+                        : "loss"
+                    }`}
+                    key={option}
+                  >
+                    {option}
+                  </li>
+                );
+              })}
             </ul>
           </div>
         ))}
-        <span>correct answers: {correctAnswers}</span>
+        {/* {correctAnswersAmount && (
+          <span>correct answers: {correctAnswersAmount}</span>
+        )} */}
       </div>
       <Button
         buttonType={BUTTON_TYPES.MAIN}
