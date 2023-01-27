30aeb3e3d525865ac0d70a5e28f1a4300b4484c2
York-Citynew <y.c.new.a@gmail.com>
2023-01-27 17:31:55 +0330

    minor changes related to the new fetching method

--- src/routes/quiz/quiz.component.jsx
+++ src/routes/quiz/quiz.component.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useMemo, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { setQuizDataDuration } from "../../utils/store/features/quiz/quiz-slice";
+import {
+  getAnswers,
+  setQuizDataDuration,
+} from "../../utils/store/features/quiz/quiz-slice";
 import Modal from "../../components/modal/modal.component";
 import QuestionBox from "../../components/question-box/question-box.component";
 import Spinner from "../../components/spinner/spinner.component";
@@ -8,13 +11,17 @@ import Spinner from "../../components/spinner/spinner.component";
 const s = 1000;
 const Quiz = () => {
   const [elapsedTime, setElapsedTime] = useState(0);
-  const { isLoading, quizData } = useSelector((state) => state.quiz);
+  const { isLoading, quizTests } = useSelector((state) => state.quiz);
+  const { user } = useSelector((state) => state.user);
   const dispatch = useDispatch();
   const [questionNumber, setQuestionNumber] = useState(0);
   useEffect(() => {
     setInterval(() => {
       setElapsedTime((prev) => (prev += 1000));
     }, 1000);
+    return () => {
+      dispatch(getAnswers(user.uid));
+    };
   }, []); // isLoading as dependency array? spinner's re-renders: the stopwatch inits before the data is fetched
   // duration is not accurate
   useEffect(() => {
@@ -24,17 +31,15 @@ const Quiz = () => {
   }, [elapsedTime]);
   const second = (elapsedTime % (60 * s)) / s;
   const minute = Math.floor(elapsedTime / (60 * s));
-  if (isLoading) {
-    return <Spinner />;
-  }
+  if (isLoading) return <Spinner />;
 
   return (
     <Modal>
       <QuestionBox
         questionNumber={questionNumber}
         setQuestionNumber={setQuestionNumber}
-        quizTests={quizData.tests[questionNumber]}
-        quizLength={quizData.tests.length}
+        quizTests={quizTests.tests[questionNumber]}
+        quizLength={quizTests.tests.length}
       />
       <span>
         {minute < 9 ? `0${minute}` : `${minute}`}:
